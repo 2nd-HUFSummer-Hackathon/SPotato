08274edbdf26c957903b8b1ba1b7de4705279b7c
luii0415 <becky45210193@gmail.com>
2023-06-24 06:23:26 +0900

    수정 1

--- domdelivery/src/Menupage/Menupage_pizza1.js
+++ domdelivery/src/Menupage/Menupage_pizza1.js
@@ -6,7 +6,7 @@ function Menupage_pizza1(props) {
     navigate(-1);
     props.setCost(14500);
   }
-  function handleClick() {
+  function handleClickTwo() {
     alert("두번째 이미지가 선택되었습니다.");
     navigate(-1);
     props.setCost(14500);
@@ -17,11 +17,11 @@ function Menupage_pizza1(props) {
       <h3>여기는 ~피자보이~입니다</h3>
       <hr></hr>
       <img
-        onClick={handleClick}
+        onClick={handleClickOne}
         src={process.env.PUBLIC_URL + `assets/pizza1-1.jpg`}
       />
       <img
-        onClick={handleClick}
+        onClick={handleClickTwo}
         src={process.env.PUBLIC_URL + `assets/pizza1-2.jpg`}
       />
     </div>
