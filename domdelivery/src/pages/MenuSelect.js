0dc5c8db28cee0478674a215037914a6112a333b
luii0415 <becky45210193@gmail.com>
2023-06-24 03:30:03 +0900

    수고해준 준성이에게 박수~~

--- domdelivery/src/pages/MenuSelect.js
+++ domdelivery/src/pages/MenuSelect.js
@@ -4,32 +4,67 @@ import Menupage_pizza3 from "../Menupage/Menupage_pizza3";
 import Menupage_chicken1 from "../Menupage/Menupage_chicken1";
 import Menupage_chicken2 from "../Menupage/Menupage_chicken2";
 import Menupage_chicken3 from "../Menupage/Menupage_chicken3";
-
+import Menupage_ham1 from "../Menupage/Menupage_ham1";
+import Menupage_ham2 from "../Menupage/Menupage_ham2";
+import Menupage_ham3 from "../Menupage/Menupage_ham3";
 function MenuSelect(props) {
   console.log(props.selectedMenuId);
-  
-  if(props.selectedMenuId == 0 || props.selectedMenuId == 1) {
-    return ( 
+  const MenuId = props.selectedMenuId;
+  if (MenuId == 0 || MenuId == 1) {
+    return (
       <div>
         <Menupage_pizza1 />
-      </div>    
+      </div>
     );
-  }
-  else if(props.selectedMenuId == 2) {
-    return ( 
+  } else if (MenuId == 2) {
+    return (
       <div>
         <Menupage_pizza2 />
-      </div>    
+      </div>
     );
-  }
-  else if(props.selectedMenuId == 3) {
-    return ( 
+  } else if (MenuId == 3) {
+    return (
       <div>
         <Menupage_pizza3 />
-      </div>    
+      </div>
+    );
+  } else if (MenuId == 4 || MenuId == 5) {
+    return (
+      <div>
+        <Menupage_chicken1 />
+      </div>
+    );
+  } else if (MenuId == 6) {
+    return (
+      <div>
+        <Menupage_chicken2 />
+      </div>
+    );
+  } else if (MenuId == 7) {
+    return (
+      <div>
+        <Menupage_chicken3 />
+      </div>
+    );
+  } else if (MenuId == 8 || MenuId == 9) {
+    return (
+      <div>
+        <Menupage_ham1 />
+      </div>
+    );
+  } else if (MenuId == 10) {
+    return (
+      <div>
+        <Menupage_ham2 />
+      </div>
+    );
+  } else if (MenuId == 11) {
+    return (
+      <div>
+        <Menupage_ham3 />
+      </div>
     );
   }
-  
 }
 
 export default MenuSelect;
