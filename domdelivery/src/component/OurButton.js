d9b3c09ab976543cca4f784be027808bc7709cf7
Batjimin <147qqa@gmail.com>
2023-06-24 00:12:05 +0900

    jj

--- domdelivery/src/component/OurButton.js
+++ domdelivery/src/component/OurButton.js
@@ -1,9 +1,19 @@
-function OurButton({ text, onclick }) {
+const OurButton = ({text,type,onClick})=>{
+    
+  const btnType = ['positive','negative'].includes(type)? type:'default';
+
   return (
-    <button className="OurButton" onClick={onclick}>
-      {text}
-    </button>
-  );
+      <button 
+          className={["OurButton",`OurButton_${btnType}`].join(" ")} 
+          onClick={onClick}
+      >
+          {text}
+      </button>
+  )
 }
+
+OurButton.defaultProps={
+  type:"default",
+};
 
 export default OurButton;
--- domdelivery/src/pages/Match.js
+++ domdelivery/src/pages/Match.js
@@ -24,10 +24,10 @@ function Match() {
         <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", marginLeft: "-20px", width:"20px"}}>1</div>
         <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px",  marginLeft:"30px", width:"400px"}}>메뉴</div>
         <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", marginLeft: "24px", width:"20px"}}>
-        <OurButton text={"O"}/>
+          <OurButton text={"O"}/>
         </div>
         <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", marginLeft: "10px", width:"20px"}}>
-        <OurButton text={"X"}/>
+          <OurButton text={"X"}/>
         </div>
       </div> 
 
