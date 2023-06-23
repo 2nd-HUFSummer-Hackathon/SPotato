8e6e31e6c7ca8b1bf08afcdeed8484a038aea1e9
Batjimin <147qqa@gmail.com>
2023-06-23 19:45:49 +0900

    --

--- domdelivery/src/pages/MenuSelect.js
+++ domdelivery/src/pages/MenuSelect.js
@@ -1,5 +1,9 @@
 function MenuSelect() {
-  return <div className="MenuSelect"></div>;
+  return (
+  <div className="MenuSelect">
+    <h2>여기는 MenuSelect</h2>
+  </div>
+  );
 }
 
 export default MenuSelect;
