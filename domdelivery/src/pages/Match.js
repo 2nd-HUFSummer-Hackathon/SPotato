d0e5bc1a163237ec0f6e305e40bc96e9c00c2bee
Batjimin <147qqa@gmail.com>
2023-06-24 03:15:29 +0900

    Match setting

--- domdelivery/src/pages/Match.js
+++ domdelivery/src/pages/Match.js
@@ -1,24 +1,21 @@
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import OurButton from "../component/OurButton";
 
-
 function Match() {
   const navigate = useNavigate();
   return (
     <div className="Match">
       <div className="Match-header">
         <div style={{display: "flex", alignItems: "flex-start", marginTop:"20px"}}>
-          <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", width:"500px"}}>0000 방</div>
-          <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", marginLeft:"50px", width:"100px"}}>
+          <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", width:"400px"}}>0000 방</div>
+          <div style={{   marginLeft:"40px", width:"100px"}}>
             <OurButton text={"메뉴 선택"} onClick={() => navigate("/menuselect")} />
           </div> 
         </div>
 
         <div style={{display: "flex", alignItems: "flex-start", marginTop:"20px"}}>
-          <div style={{color:"white", backgroundColor:"blue", padding:"20px",width:"500px"}}>현재 모인 돈 / 최소 주문 금액</div>
-          <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", marginLeft:"50px", width:"100px"}}>
-            <span> ${} / ${} </span>
-          </div>
+          <div style={{color:"white", backgroundColor:"blue", padding:"20px",width:"800px"}}>현재 모인 돈 / 최소 주문 금액</div>
+          
         </div>
       </div>
 
