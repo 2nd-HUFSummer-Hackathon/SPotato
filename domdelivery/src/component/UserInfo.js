add05309260b99d589dc4f5c6ea9ee31b1fde77f
Batjimin <147qqa@gmail.com>
2023-06-23 23:56:16 +0900

    Modify Error

--- domdelivery/src/component/UserInfo.js
+++ domdelivery/src/component/UserInfo.js
@@ -2,16 +2,21 @@ import { useState } from "react";
 
 function UserInfo() {
   const [text, setText] = useState("");
+  const [code,setCode] = useState("");
 
   const onChange = (e) => {
     setText(e.target.value);
+  };
+
+  const onChangeCode = (e)=>{
+    setCode(e.target.value);
   };
 
   return (
     <div className="UserInfo">
       <h3 className="guest">게스트 회원 정보를 입력해줘</h3>
       <input placeholder='닉네임' onChange={onChange} value={text} />
-      <input placeholder='환불계좌' onChange={onChange} value={text} />
+      <input placeholder='환불계좌' onChange={onChangeCode} value={code} />
     </div>
   );
 }
