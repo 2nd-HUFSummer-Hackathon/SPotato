adc82ee44a17912007e0e70ffe89ace51f29ec02
Batjimin <147qqa@gmail.com>
2023-06-24 02:30:56 +0900

    Delivery complete

--- domdelivery/src/pages/Delivery.js
+++ domdelivery/src/pages/Delivery.js
@@ -14,10 +14,17 @@ const Delivery=()=> {
         clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머 제거
       };
     }, []); // 컴포넌트가 처음 렌더링될 때만 실행
+
+    const text = currentImage===process.env.PUBLIC_URL+`assets/line2.png` 
+      ? "주문하신 음식이 20분 내로 도착 예정입니다. 교내 배달존으로 모여주세요!" 
+      : "음식이 조리 중입니다. 조금만 기다려 주세요... ."
   
     return (
       <div>
-        <img  className="imgline" src={currentImage} alt="Current Image" />
+        <img className="imgline" src={currentImage} alt="Current Image" />
+        <div className="delivery-text">
+          {text}
+        </div>
       </div>
     );
   };
@@ -26,10 +33,7 @@ const Delivery=()=> {
     <div className="Delivery">
       <img className="bike" src={process.env.PUBLIC_URL+`assets/bike.png`}/>
       <div ><ImageChanger/></div>
-      <div className="delivery-text">
-        주문하신 음식이 n분 후에 도착 예정입니다.
-        교내 배달존으로 모여주세요.
-      </div>
+      
     </div>
   )
 }
