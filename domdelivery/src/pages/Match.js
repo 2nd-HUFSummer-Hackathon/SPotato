33649c97424ba4bdcd9ff1d6a78bd871efecdddc
Parkjunesong <parkjunesong@gmail.com>
2023-06-23 23:09:23 +0900

    update

--- domdelivery/src/pages/Match.js
+++ domdelivery/src/pages/Match.js
@@ -5,7 +5,7 @@ import OurButton from "../component/OurButton";
 function Match() {
   const navigate = useNavigate();
   return (
-    <div className="Match" style={{display: "inline-block", verticleAlign: "middle"}}>
+    <div className="Match">
       <div style={{display: "flex", alignItems: "flex-start"}}>
        <div style={{marginTop: 20, width: 450}}>
          <h2>___ 방</h2>
@@ -18,6 +18,7 @@ function Match() {
       <div style={{marginLeft: 20, marginTop: 20}}>
         <h3 style={{fontSize: 20}}>현재 모인 돈 / 최소 주문 금액</h3>
       </div>
+
     </div>
   );
 }
