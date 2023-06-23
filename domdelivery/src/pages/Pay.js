1c195eaf741f9472d82a3da4a24b4b393a35bd54
Parkjunesong <parkjunesong@gmail.com>
2023-06-23 21:41:35 +0900

    pay update

--- domdelivery/src/pages/Pay.js
+++ domdelivery/src/pages/Pay.js
@@ -1,7 +1,27 @@
 function Pay() {
   return (
     <div className="Pay">
-      <h4>여기는 pay</h4>
+      <div className="Pay-header">
+        <h1>가상 계좌: ~~~~~~~~</h1>
+        <h1>N 원</h1>
+      </div>
+      
+      <hr></hr>
+
+      <div className="Pay-main" align="center">
+        <tr>
+          <td>1</td>
+          <td>메뉴</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>메뉴</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>메뉴</td>
+        </tr>    
+      </div> 
     </div>
   );
 }
