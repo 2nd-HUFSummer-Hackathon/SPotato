58d475554c1acf197607f44b0371346e3bbc996c
luii0415 <becky45210193@gmail.com>
2023-06-24 01:58:47 +0900

    저는 이거 먹을래요

--- domdelivery/src/Menupage/Menupage_chicken2.js
+++ domdelivery/src/Menupage/Menupage_chicken2.js
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
 function Menupage_chicken2() {
+  const navigate = useNavigate();
+  function handleClick() {
+    alert("이미지가 선택되었습니다.");
+    navigate(-1);
+  }
   return (
     <div className="MenuSelect">
       <h3>여기는 ~교촌치킨~입니다</h3>
-      <img src={process.env.PUBLIC_URL + `assets/chicken2-1.jpg`} />
-      <img src={process.env.PUBLIC_URL + `assets/chicken2-2.jpg`} />
+      <img
+        onClick={handleClick}
+        src={process.env.PUBLIC_URL + `assets/chicken2-1.jpg`}
+      />
+      <img
+        onClick={handleClick}
+        src={process.env.PUBLIC_URL + `assets/chicken2-2.jpg`}
+      />
     </div>
   );
 }
--- domdelivery/src/Menupage/Menupage_ham3.js
+++ domdelivery/src/Menupage/Menupage_ham3.js
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
 function Menupage_ham3() {
+  const navigate = useNavigate();
+  function handleClick() {
+    alert("이미지가 선택되었습니다.");
+    navigate(-1);
+  }
   return (
     <div className="MenuSelect">
       <h3>여기는 ~666버거~입니다</h3>
-      <img src={process.env.PUBLIC_URL + `assets/ham3-1.jpg`} />
-      <img src={process.env.PUBLIC_URL + `assets/ham3-2.jpg`} />
+      <img
+        onClick={handleClick}
+        src={process.env.PUBLIC_URL + `assets/ham3-1.jpg`}
+      />
+      <img
+        onClick={handleClick}
+        src={process.env.PUBLIC_URL + `assets/ham3-2.jpg`}
+      />
     </div>
   );
 }
