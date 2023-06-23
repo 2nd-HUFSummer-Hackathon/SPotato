2bce3f5116ed8aa27ff6db9e86a7104682b37998
luii0415 <becky45210193@gmail.com>
2023-06-24 04:58:58 +0900

    우리 이거 먹을 거에요

--- domdelivery/src/pages/Match.js
+++ domdelivery/src/pages/Match.js
@@ -2,14 +2,37 @@ import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import OurButton from "../component/OurButton";
 
 function Match(props) {
-  const RoomName = props.selectedMenuId;
+  const number = props.selectedMenuId;
+  let RoomName = "미선택";
+  if (number == 0 || number == 1) {
+    RoomName = "피자보이";
+  } else if (number == 2) {
+    RoomName = "반올림피자";
+  } else if (number == 3) {
+    RoomName = "베이식스";
+  } else if (number == 4 || number == 5) {
+    RoomName = "미파닭";
+  } else if (number == 6) {
+    RoomName = "교촌치킨";
+  } else if (number == 7) {
+    RoomName = "BHC";
+  } else if (number == 8 || number == 9) {
+    RoomName = "롯데리아";
+  } else if (number == 10) {
+    RoomName = "맘스터치";
+  } else if (number == 11) {
+    RoomName = "666버거";
+  } else if (number == 12) {
+    RoomName = "피자보이";
+  }
 
   const navigate = useNavigate();
+  console.log(RoomName);
   return (
     <div className="Match">
       <div className="Match-header">
         <div className="RoomInfo">
-          <div className="RoomName">{} 방</div>
+          <div className="RoomName">{RoomName} 방</div>
           <div style={{ marginLeft: "40px", width: "100px" }}>
             <OurButton
               text={"메뉴 선택"}
