c38c5e8bff305ebb9432ef6c6a0061995f5136ca
luii0415 <becky45210193@gmail.com>
2023-06-23 19:37:48 +0900

    에러 확인

--- domdelivery/src/App.js
+++ domdelivery/src/App.js
@@ -1,34 +1,28 @@
 import "./App.css";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home'
-import Match from './pages/Match'
-import MenuSelect from './pages/MenuSelect'
-import Pay from './pages/Pay'
-import Delivery from './pages/Delivery'
+import Home from "./pages/Home";
+import Match from "./pages/Match";
+import MenuSelect from "./pages/MenuSelect";
+import Pay from "./pages/Pay";
+import Delivery from "./pages/Delivery";
 
-
-
-const dummyData = [
-  {
-  },
-
-]
+const dummyData = [{}];
 
 function App() {
   return (
     <BrowserRouter>
-          <div className="App">
-            <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/match" element={<Match/>} />
-              <Route path="/menuselect/:id" element={<MenuSelect/>} />
-              <Route path="/pay" element={<Pay/>} />
-              <Route path="/delivery" element={<Delivery/>}/>
-            </Routes>
-          </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/match" element={<Match />} />
+          <Route path="/menuselect/:id" element={<MenuSelect />} />
+          <Route path="/pay" element={<Pay />} />
+          <Route path="/delivery" element={<Delivery />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
---
+++ domdelivery/src/component/OurButton.js
@@ -0,0 +1,5 @@
+function OurButton() {
+  return <div></div>;
+}
+
+export default OurButton;
