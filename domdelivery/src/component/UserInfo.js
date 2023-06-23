b81968dda4bb852a8dd20cc4f99ee322e5564534
luii0415 <becky45210193@gmail.com>
2023-06-23 20:33:52 +0900

    luii 중간 수정

---
+++ domdelivery/src/component/UserInfo.js
@@ -0,0 +1,10 @@
+function UserInfo() {
+  return (
+    <div className="UserInfo">
+      <h4>여기는 UserInfo</h4>
+      <input />
+    </div>
+  );
+}
+
+export default UserInfo;
--- domdelivery/src/pages/Home.js
+++ domdelivery/src/pages/Home.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import OurButton from "../component/OurButton";
+import UserInfo from "../component/UserInfo";
 function Home() {
   const navigate = useNavigate();
   return (
     <div className="Home">
       <h3>오류 안뜬당 히히</h3>
       <h4>여기는 home</h4>
+      <UserInfo />
       <OurButton text={"밥 먹으러 가자!"} onclick={() => navigate("/match")} />
     </div>
   );
