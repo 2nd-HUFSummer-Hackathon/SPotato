3b25f35f6147ca8a4b910b9533ca3b161edf2a9a
Parkjunesong <parkjunesong@gmail.com>
2023-06-23 23:50:54 +0900

    pay update

--- domdelivery/src/pages/Pay.js
+++ domdelivery/src/pages/Pay.js
@@ -2,15 +2,13 @@ function Pay() {
   return (
     <div className="Pay">
       <div className="Pay-header">
-        <div style={{color:"white", backgroundColor:"blue", padding:"20px",marginLeft:"200px",marginRight:"200px",marginBottom:"10px"}}>가상계좌: ~~~</div>
-        <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", marginLeft:"220px",marginRight:"220px"}}>N 원</div>
+        <div style={{color:"white", backgroundColor:"blue", padding:"20px",marginLeft:"100px",marginRight:"100px",marginBottom:"10px"}}>가상계좌: ~~~</div>
+        <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", marginLeft:"120px",marginRight:"120px"}}>N 원</div>
       </div>
-
-      <br></br>
 
       <div className="Pay-main">
         <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", width:"20px"}}>1</div>
-        <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px",  marginLeft:"50px", width:"500px"}}>메뉴</div>
+        <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px",  marginLeft:"50px", width:"300px"}}>메뉴</div>
       </div> 
     </div>
   );
