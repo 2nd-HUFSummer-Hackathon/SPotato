7171549f62834f111799657e7ec5d3eccaebf189
Batjimin <147qqa@gmail.com>
2023-06-24 01:51:35 +0900

    btn

--- domdelivery/src/component/OurButton.js
+++ domdelivery/src/component/OurButton.js
@@ -1,19 +1,29 @@
-const OurButton = ({text,type,onClick})=>{
-    
-  const btnType = ['positive','negative'].includes(type)? type:'default';
+import React, { useState } from 'react';
+
+const OurButton = ({ text, type, onClick }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    if (onClick) {
+      onClick(); // onClick prop이 존재할 경우 해당 함수를 호출합니다.
+    }
+  };
 
   return (
-      <button 
-          className={["OurButton",`OurButton_${btnType}`].join(" ")} 
-          onClick={onClick}
-      >
-          {text}
-      </button>
-  )
-}
+    <button
+      className={`OurButton ${isClicked ? 'OurButton_clicked' : ''}`}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  );
+};
 
-OurButton.defaultProps={
-  type:"default",
+OurButton.defaultProps = {
+  type: 'default',
 };
 
 export default OurButton;
+
+
--- domdelivery/src/pages/Match.js
+++ domdelivery/src/pages/Match.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import OurButton from "../component/OurButton";
 
+const OrderCount = () => {
 
+};
 
 
 function Match() {
@@ -19,7 +21,7 @@ function Match() {
         <div style={{display: "flex", alignItems: "flex-start", marginTop:"20px"}}>
           <div style={{color:"white", backgroundColor:"blue", padding:"20px",width:"500px"}}>현재 모인 돈 / 최소 주문 금액</div>
           <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px", marginLeft:"50px", width:"100px"}}>
-            <OurButton text={"주문하기"}/>
+            <span> ${} / ${} </span>
           </div>
         </div>
       </div>
@@ -29,9 +31,6 @@ function Match() {
         <div style={{borderWidth: 2, borderColor:'green', borderStyle:"solid", padding:"20px",  marginLeft:"30px", width:"400px"}}>메뉴</div>
         <div style={{padding:"20px", marginLeft: "10px", width:"20px"}}>
           <OurButton type='positive' text={"O"} />
-        </div>
-        <div style={{ padding:"20px", marginLeft: "10px",marginRight:"10px", width:"20px"}}>
-          <OurButton type='negative' text={"X"}/>
         </div>
       </div> 
 
