b3562a06a818452fdba02bfd710b9c19a3285c2b
Batjimin <147qqa@gmail.com>
2023-06-23 19:30:19 +0900

    Router

--- domdelivery/src/App.js
+++ domdelivery/src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import Home from './pages/Home'
+import Match from './pages/Match'
+import MenuSelect from './pages/MenuSelect'
+import Pay from './pages/Pay'
+import Delivery from './pages/Delivery'
+
+
+
+const dummyData = [
+  {
+  },
+
+]
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/match" element={<Match/>} />
+              <Route path="/menuselect/:id" element={<MenuSelect/>} />
+              <Route path="/pay" element={<Pay/>} />
+              <Route path="/delivery" element={<Delivery/>}/>
+            </Routes>
+          </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
